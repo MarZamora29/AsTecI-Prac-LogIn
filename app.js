834ec1af1aced811fd96ff8e7f5be2cb1a186ec6
MarZamora29 <mariel.zamora99@outlook.com>
2025-09-26 17:07:20 -0600

    Agregamos un evento al formulario, evitamos que el formulario recargue la página al hacer submit, mostrar mensajes de error o éxito, y const de valores que agrego el usuario

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 const form = document.getElementById("loginForm");
+const form = document.getElementById("message");
+
+const validEmail = "correo@dominio.com";
+const validPassword = "123";
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+})
