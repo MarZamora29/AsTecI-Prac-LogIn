dc10be230c87e7af45a72dd3133a26fcf2333b80
MarZamora29 <mariel.zamora99@outlook.com>
2025-09-29 22:30:26 -0600

    EXTRAS validaciones en contraseña, al menos una letra mayúscula y redirigirse a una nueva página -> bienvenida.html

--- app.js
+++ app.js
@@ -2,7 +2,21 @@ const form = document.getElementById("loginForm");
 const message = document.getElementById("message");
 
 const validEmail = "test@correo.com";
-const validPassword = "123456";
+const validPassword = "123456A"; //E: agregue "A"-mayúscula
+
+//E: Función para validar contraseña-------------------------
+function validarPassword(password) {
+  const minLength = 6;
+  const tieneMayuscula = /[A-Z]/.test(password); // al menos con una mayúscula
+  if (password.length < minLength) {
+    return "La contraseña debe tener al menos 6 caracteres";
+  }
+  if (!tieneMayuscula) {
+    return "La contraseña debe incluir al menos una letra mayúscula";
+  }
+  return ""; // vacío si no hay errores
+}
+//E: ------------------------------------------------------
 
 form.addEventListener("submit", function(e) {
     e.preventDefault();
@@ -10,9 +24,23 @@ form.addEventListener("submit", function(e) {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
+//E: ------------------------------------------------------
+// Validar contraseña antes de checar usuario/contraseña correctos
+const errorPassword = validarPassword(password);
+if (errorPassword) {
+    message.textContent = errorPassword;
+    message.style.color = "red";
+    return; //E: si falla la validación aquí se detiene
+}
+//E:-------------------------------------------------------
+
     if (email == validEmail && password == validPassword) {
-        message.textContent= "login exitoso";
+        message.textContent= "login exitoso, redirigiendo..."; //E:se agregó: redirigiendo...
         message.style.color = "green";
+        //E: redirección ---------------------------------
+        setTimeout(() => {
+            window.location.href = "bienvenida.html"; 
+        }, 2000); //E:-------------------------------------
     } else {
         message.textContent = "Usuario o contraseña incorrectos";
         message.style.color = "red";
